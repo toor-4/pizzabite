fb95cc8b3ffd8d5399588f6a4cf68f355e380137
alfred <frealchar@gmail.com>
2025-02-23 08:17:49 +0800

    Add basic views and URL routing for pizza app with home and about pages

--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'pizza/index.html')
+
+
+def about(request):
+    return render(request, 'pizza/about.html')
