7843dbca6921129736d85a67e2a15451b3858bde
alfred <frealchar@gmail.com>
2025-02-24 22:27:49 +0800

    Add theme app with Tailwind CSS integration and initial setup

--- pizzabite/urls.py
+++ pizzabite/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pizza.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
     
